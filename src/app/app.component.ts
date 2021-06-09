3d4db8f52e981c348e23cf1aa0810bc4326d2835
Juan Carlos Sanchez Solorzano <jsancsol@everis.com>
2021-06-09 17:39:06 -0500

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-appEveris';
+
+  public name: string = 'Juan Carlos';
+  public lastname: string = 'Sanchez';
+  public birthdate: string = '1995-06-24';
+
+  constructor(){
+
+  }
+
+  public onNameChanged(ev: any){
+    this.name = ev.target.value;
+  }
+
+  public onLastnameChanged(ev: any){
+    this.lastname = ev.target.value;
+  }
+
+  public onBirthdateChanged(ev: any){
+    this.birthdate = ev.target.value;
+  }
 }
