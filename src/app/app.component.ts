84d4e5f445c2065e0fd4276afb3951b32ea79a21
Eduardo Zabala <jzabalag1@ucentral.edu.co>
2021-06-09 17:46:29 -0500

    Se agrega componente Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public name: string = 'Jaime';
+  public lastname: string = 'Zabala';
+  public birthdate: string = '1998-11-30';
+  
   title = 'proyectangul';
+  constructor() { }
+
+  public onNameChanged(ev: any){
+  this.name=ev.target.value;
+  }
+  public onLastnameChanged(ev: any){
+
+    this.lastname=ev.target.value;
+  }
+  public onBirthdateChanged(ev: any){
+
+    this.birthdate=ev.target.value;
+  }
 }
