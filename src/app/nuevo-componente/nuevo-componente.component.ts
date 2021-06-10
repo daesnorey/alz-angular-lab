e8ab5751e0297e702a8f1a4728df7ccfcdc39689
Jeison R <rojasmjeison@gmail.com>
2021-06-10 15:46:16 -0500

    Se agregan eventos de prueba

--- src/app/nuevo-componente/nuevo-componente.component.ts
+++ src/app/nuevo-componente/nuevo-componente.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NuevoComponenteComponent implements OnInit {
 
+  public nombre: string = "Sebastian";
+  public apellido: string = "Vettel";
+  public fechaNacimiento: string = "1987-07-03";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  //Se crean eventos de prueba
+  public cambioNombre(evento:any){
+    this.nombre = evento.target.value;
+  }
+
+  public cambioApellido(evento:any){
+    this.apellido = evento.target.value;
+  }
+
+  public cambioFecha(evento:any){
+    this.fechaNacimiento = evento.target.value;
   }
 
 }
