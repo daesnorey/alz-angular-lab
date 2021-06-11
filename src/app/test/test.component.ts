aad2575b1929a7aca51905972b5af10f7e3a4db9
juancarlos <jckbron@gmail.com>
2021-06-11 14:24:46 -0500

    Se agregan pruebas

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,8 +7,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {  }
 
   @Input()
   public name?: String ;
