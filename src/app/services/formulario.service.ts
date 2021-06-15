1530f6cf46dde1cb842248902245bd8e788a5173
Joan Sebastian Martinez Moreno <jmamoren@everis.com>
2021-06-15 17:25:01 -0500

    Nuevo servicio para formularios

---
+++ src/app/services/formulario.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormularioService {
+
+  private persons : any[] = []
+
+  /**
+   * newPerson
+   */
+  public newPerson(_persona : any) {
+    this.persons.push(_persona)
+  }
+ 
+  /**
+   * getPersons
+   */
+   public getPersons() {
+    return this.persons;
+  }
+
+  constructor() { }
+}
