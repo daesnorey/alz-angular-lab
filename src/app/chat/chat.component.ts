461caa6d417a7bcb9137cf4e59951100f09e27ed
Eduardo Zabala <jzabalag1@ucentral.edu.co>
2021-06-16 17:15:34 -0500

    instancio el servicio y agrego el providers

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 import { ChatService } from './chat.service';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.css'],
+  providers:[
+    DataService,
+    ChatService
+  ]
 })
 export class ChatComponent implements OnInit {
   public message: string= '';
 
   constructor(
-    private chatService: ChatService  
+    private chatService: ChatService,
+    private dataService: DataService 
   ) { }
 
   ngOnInit(): void {
