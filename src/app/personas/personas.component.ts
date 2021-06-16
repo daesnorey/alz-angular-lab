e33d25d757e83ccc559f2868ff76640aea879ece
Eduardo Zabala <jzabalag1@ucentral.edu.co>
2021-06-16 17:19:03 -0500

    no es necesario importar el servicio en los componentes hijos

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import { ChatService } from '../chat/chat.service';
+import { DataService } from '../data.service';
 
 
 @Component({
@@ -13,6 +14,7 @@ export class PersonasComponent implements OnInit {
   
   constructor(
     public chatService: ChatService
+
   ) {
 
   }
