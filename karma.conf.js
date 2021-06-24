09a2e32da40675077ec16a41b116e02645ba3573
Nicolas Chaves <nicolas.chaves93@gmail.com>
2021-06-24 17:07:55 -0500

    iniciar

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/my-nicolas'),
+      dir: require('path').join(__dirname, './coverage/practica'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-nicolas'`, () => {
+  it(`should have as title 'practica'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-nicolas');
+    expect(app.title).toEqual('practica');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('my-nicolas app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('practica app is running!');
   });
 });
